6cc3f0f56b71d4f4847943df59aacab621d463ac
Aman Gulati <amangulati075@gmail.com>
2018-11-27 11:28:44 +0530

    changed constants for more testing

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,10 +1,10 @@
 const Constants = {
     DROP_DOWN_DATA: [
-        { "id": 738491, "name": "value1" },
-        { "id": 738492, "name": "value2" },
-        { "id": 738493, "name": "value3" },
-        { "id": 738494, "name": "value4" },
-        { "id": 738495, "name": "value5" },
+        { "id": 738491, "name": "Avalue1" },
+        { "id": 738492, "name": "Bvalue2" },
+        { "id": 738493, "name": "AvaBlue3" },
+        { "id": 738494, "name": "Cvalue4" },
+        { "id": 738495, "name": "Dvalue5" },
     ]
 }
 
