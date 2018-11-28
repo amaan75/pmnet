abc51330c8c4690a9fd45554d58e8e3072de281f
Aman Gulati <amangulati075@gmail.com>
2018-11-28 10:49:35 +0530

    updated the select with new and correct styles

--- src/components/App.js
+++ src/components/App.js
@@ -24,17 +24,33 @@ class App extends Component {
   onChangeDropDownValue = (option, { action }) => {
     if (action === 'select-option')
       this.setState({ dropdownValue: option })
-
   }
+
+
   render() {
+    const customStyles = {
+      option: (provided, state) => ({
+        ...provided,
+        '&:hover': {
+          background: "rgba(0, 0, 255, 0.1)"
+        },
+        background: "white",
+        color: "black"
+      })
+    }
     return (
       this.state.show &&
       <div className="container">
         <Select
+          styles={customStyles}
           isSearchable={true}
           options={this.state.dropdownData}
           getOptionLabel={option => option.name}
+          isClearable={false}
+          backspaceRemovesValue={true}
           onChange={this.onChangeDropDownValue}
+          inputValue={this.state.dropdownInputTextValue}
+          onInputChange={(inputValue, { action }) => this.setState({ dropdownInputTextValue: inputValue })}
           value={this.state.dropdownValue}
         // isDisabled={this.props.templateDisabled}
         />
