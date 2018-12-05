1c78d06ca724a6d8a23c8c4a4d0db45729456842
Aman Gulati <amangulati075@gmail.com>
2018-12-05 08:12:10 +0530

    added overflow style

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 const Constants = {
     DROP_DOWN_DATA: [
-        { "id": 738491, "name": "Avalue1" },
+        { "id": 738491, "name": "Avalue1kbhkubjkhiugiuguihiuguyguigbfuhibnyufjbnjkvhvgvhjbhjvghvhjb hv ghv hj bhjv gh" },
         { "id": 738492, "name": "Bvalue2" },
         { "id": 738493, "name": "AvaBlue3" },
         { "id": 738494, "name": "Cvalue4" },
